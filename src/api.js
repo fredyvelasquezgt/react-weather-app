9cfbc387ae5678c4cd8c876adfbb2c49c50a213c
Fredy Velasquez <macacoloco696@gmail.com>
2022-07-21 00:34:35 -0600

    WIP: GeoAPI added

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "98018f6dd9msh16f9c3b9e67859fp112698jsn6ccb5ee0ea6d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
