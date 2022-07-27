74c08cb56399aff3359b5f3dc15840a53785ac62
Fredy Velasquez <macacoloco696@gmail.com>
2022-07-26 23:17:57 -0600

    WIP: Render AsyncPaginate component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,21 +5,19 @@ import { GEO_API_URL, geoApiOptions } from "../../api";
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
-
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix={inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
       .then((response) => console.log(response))
       .catch((err) => console.error(err));
   };
-
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
   return (
     <AsyncPaginate
       placeholder="Search for city"
